91feb89dc5e1925907218dae2bbc69a4d51efdba
DanZx819 <147755543+DanZx819@users.noreply.github.com>
2024-12-03 15:12:17 -0300

    Commit

--- app/src/main/java/com/example/gorjeta/MainActivity.kt
+++ app/src/main/java/com/example/gorjeta/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.gorjeta
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.RadioButton
 import android.widget.TextClock
@@ -11,16 +14,20 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.gorjeta.databinding.ActivityMainBinding
 import com.google.android.material.chip.Chip
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("SetTextI18n", "MissingInflatedId")
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -28,26 +35,70 @@ class MainActivity : AppCompatActivity() {
         }
 
         val edtPreco = findViewById<TextInputEditText>(R.id.edt_preco)
-        val edtPessoas = findViewById<TextInputEditText>(R.id.edt_pessoas)
         val tvResult = findViewById<TextView>(R.id.tv_result)
         val btnCalc = findViewById<Button>(R.id.btn_calc)
-        val edtGorjeta = findViewById<TextInputEditText>(R.id.edt_gorjeta)
         val tvResult2 = findViewById<TextView>(R.id.tv_result2)
         val btnReset = findViewById<Button>(R.id.btn_reset)
+
+        val adapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.num_people,
+            android.R.layout.simple_spinner_item
+        )
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.spinnerPeople.adapter = adapter
+
+        var num_people = 0
+        binding.spinnerPeople.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long)
+            {
+                num_people = position
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+
+        }
+
+        var percentage: Int = 0
+        binding.rbOptionOne.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked){
+                percentage = 10
+            }
+        }
+        binding.rbOptionTwo.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked){
+                percentage = 15
+            }
+        }
+        binding.rbOptionThree.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked){
+                percentage = 20
+            }
+        }
+
 
 
         btnCalc.setOnClickListener() {
             val StrPreco = edtPreco.text.toString()
-            val StrPessoas = edtPessoas.text.toString()
-            val StrGorjeta = edtGorjeta.text.toString()
-            if (StrPreco.isNotEmpty() && StrPessoas.isNotEmpty() && StrGorjeta.isNotEmpty()) {
+
+
+            if (StrPreco.isNotEmpty()  ) {
                 val flPreco = StrPreco.toFloat()
-                val flPessoas = StrPessoas.toFloat()
-                val flGorjeta = StrGorjeta.toFloat()
-                val resultado1 = flPreco / flPessoas
-                val resultado2 = (resultado1 * flGorjeta) / 100
+
+                val resultado1 = flPreco / num_people
+                val resultado2 = (resultado1 * percentage) / 100
+                val resultado3 = (flPreco * percentage / 100)
                 tvResult.text = "R$%.2f".format(resultado1)
-                tvResult2.text = "R$%.2f".format(resultado2, flGorjeta)
+                tvResult2.text = "R$%.2f".format(resultado2)
+                binding.tvResult3.text = "R$%.2f".format(resultado3)
             } else {
                 Snackbar
                     .make(
@@ -59,9 +110,14 @@ class MainActivity : AppCompatActivity() {
 
         }
         btnReset.setOnClickListener() {
-            tvResult.text = ""
-            tvResult2.text = ""
-        }
+            tvResult.text = "R$0,00"
+            tvResult2.text = "R$0,00"
 
+            binding.edtPreco.setText("")
+            binding.tvResult3.text = "R$0,00"
+            binding.rbOptionOne.isChecked = false
+            binding.rbOptionTwo.isChecked = false
+            binding.rbOptionThree.isChecked = false
+        }
     }
 }
